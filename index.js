f58e385fcfb78caa5a46ed157cefd155a42a22ca
Vrentadax <vrentadax@outlook.com>
2022-04-13 11:12:44 -0600

    rephrased pseudocode for easier comprehention for me

---
+++ index.js
@@ -0,0 +1,15 @@
+//inquirer (select*all departments/roles/employees)(add department)
+
+//department = id/name
+
+//role = id/title/salary/department_id
+
+//employee = id/first_name/last_name/role_id/manager_id
+
+//add department = enter name of department += table
+
+//add role = enter name/salary/department
+
+//add employee = first/last/role/manager
+
+//update employee role = select employee to update and new role is updated
