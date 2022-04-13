191825b887e8cf075ccb67fb3ea8dfcaea7ddb33
Vrentadax <vrentadax@outlook.com>
2022-04-13 17:29:47 -0600

    bulk of add functions in place, couple minor bugs to work out

--- index.js
+++ index.js
@@ -102,15 +102,123 @@ const init = () => {
     };
 
     const addDepartment = () => {
-
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'new_department',
+                message: 'What is the new department called?'
+            }
+        ]).then(input => {
+            const sql = `INSERT INTO departments(department) VALUES (?)`;
+            const params = input.new_department;
+            db.query(sql, params, (err, result) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(`Added ${params} to the database`);
+                mainMenu();
+            });
+        });
     };
 
     const addRole = () => {
-
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'new_role',
+                message: 'What is the new role called?'
+            },
+            {
+                type: 'input',
+                name: 'new_department_id',
+                message: `What is the new role's department id?`,
+                validate: input => {
+                    if(!isNaN(input)) {
+                        return true;
+                    } else {
+                        console.log(' Please enter a number');
+                        return false;
+                    };
+                }
+            },
+            {
+                type: 'input',
+                name: 'new_salary',
+                message: `What is the new role's salary?`,
+                validate: input => {
+                    if(isNaN(input)) {
+                        console.log('Please enter a number');
+                        return false;
+                    } else {
+                        return true;
+                    };
+                }
+            }
+        ]).then(input => {
+            const sql = `INSERT INTO roles(title, department_id, salary) VALUES (?,?,?)`;
+            const params = [input.new_role, input.new_department_id, input.new_salary];
+            db.query(sql, params, (err, result) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(`Added ${params[0]} to the database`);
+                mainMenu();
+            });
+        });
     };
 
     const addEmployee = () => {
-
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: `What is the new employee's first name?`
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: `What is the new employee's last name?`
+            },
+            {
+                type: 'input',
+                name: 'role_id',
+                message: `What is the new employee's role id?`,
+                validate: input => {
+                    if(!isNaN(input)) {
+                        return true;
+                    } else {
+                        console.log(' Please enter a number');
+                        return false;
+                    };
+                }
+            },
+            {
+                type: 'input',
+                name: 'manager_id',
+                message: `What is the new employee's manager id?`,
+                validate: input => {
+                    if(!isNaN(input)) {
+                        return true;
+                    } else {
+                        console.log(' Please enter a number');
+                        return false;
+                    };
+                }
+            }
+        ]).then(input => {
+            const sql = `INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+            const params = [input.first_name, input.last_name, input.role_id, input.manager_id];
+            db.query(sql, params, (err, result) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(`Added ${params[0]} ${params[1]} to the database`);
+                mainMenu();
+            });
+        });
     };
 
     const updateRole = () => {
